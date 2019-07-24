37383af736de96eb85a72649511423b78b232ffa
ap-systems <dylanb@ap-acounting.co.za>
2019-07-24 11:02:31 +0200

    adding debuggers to trace differences between unit standards lists

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -9641,7 +9641,7 @@ class provider_assessment(models.Model):
 						this_us_list.append(us)
 				if achieved_ids.moderators_id:
 					for mod_qualifications in achieved_ids.moderators_id.moderator_qualification_ids:
-						for mod_us in mod_qualifications.quallification_line_hr:
+						for mod_us in mod_qualifications.qualification_line_hr:
 							this_mod_us_list.append(mod_us)
 				if achieved_ids.assessors_id:
 					for ass_qualifications in achieved_ids.assessors_id.qualification_ids:
