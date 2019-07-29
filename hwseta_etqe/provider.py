997d7b36f8ba2642ce8e87927f1263b01fa0fb09
ap-systems <dylanb@ap-acounting.co.za>
2019-07-29 14:54:40 +0200

    lib checker dbg statements

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -9653,7 +9653,7 @@ class provider_assessment(models.Model):
 			for libz in list_of_dict:
 				text_guy += "<div>" + str(libz.get('code')) + "</div>"
 				for us in libz.get('list_of_us'):
-					text_guy += "<div>" + us + "</div>"
+					text_guy += "<div>" + str(us) + "</div>"
 			# text_guy += "<h1>Library:</h1>"
 			# text_guy += "<h3>In assessment, not in Library:</h3>"
 			# for x in lib_diff:
