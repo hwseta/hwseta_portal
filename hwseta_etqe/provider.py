80eda5c3285239051bafc5ce2151e27251f06d21
ap-systems <dylanb@ap-acounting.co.za>
2019-07-31 15:27:47 +0200

    start to lib check on provider.accreditation

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -6276,10 +6276,9 @@ class provider_accreditation(models.Model):
 			for prov_quals in self.qualification_ids:
 				quals_dict.update({prov_quals:[]})
 				for prov_us in prov_quals.qualification_line:
-					if prov_us.id_data not in this_prov_us_list and prov_us.selection:
-						list_of_quals.get(prov_quals).append(prov_us.id_data)
+					if prov_us.id_no not in this_prov_us_list and prov_us.selection:
+						list_of_quals.get(prov_quals).append(prov_us.id_no)
 						# this_prov_us_list.append([x.id_data for x in prov_us])
-						this_prov_us_list.append(prov_us.id_data)
 			dbg(quals_dict)
 
 
