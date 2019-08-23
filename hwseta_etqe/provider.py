54b1d727241422061b7b54346e0d75417b88f3db
ap-systems <dylanb@ap-acounting.co.za>
2019-08-23 14:42:03 +0200

    added learner stuff to fix def

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -10856,6 +10856,7 @@ class provider_assessment(models.Model):
 				learner = ass_qual_line.learner_id
 				mod = ass_qual_line.moderators_id
 				ass = ass_qual_line.assessors_id
+				raise Warning(_(qual_dict.get(qual_id.saqa_qual_id)))
 				for reg_qual in learner.learner_qualification_ids:
 					# raise Warning(_(str(reg_qual.learner_qualification_parent_id.saqa_qual_id) + '\n' + str(qual_id)))
 					if reg_qual.batch_id == batch and reg_qual.learner_qualification_parent_id.saqa_qual_id == qual_id.saqa_qual_id:
