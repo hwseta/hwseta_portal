6b2f4098e7827f686137d2a3ffcbb494853030df
ap-systems <dylanb@ap-acounting.co.za>
2019-08-26 10:42:44 +0200

    added learner stuff to fix def

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -11074,7 +11074,7 @@ class provider_assessment(models.Model):
 									text_guy += '!!!!!!!!!learner NOT achieved\n'
 								# 	dbg(str(line) + 'selected line' + str(selected_line) + 'achieved line:' + str(achieved_line))
 						learner_achieved.append((0, 0, learner_dict))
-				raise Warning(_(text_guy))
+				# raise Warning(_(text_guy))
 			assessment_status_obj = self.env['assessment.status'].create({'name': self._uid,
 																  'state':'achieved',
 																  'pro_id':self.id,
